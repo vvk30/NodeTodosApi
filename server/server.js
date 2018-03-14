09413946d1c02b157633a497dbfde5d33f7b7dfb
Vishal Kulkarni <vishal.kulkarni303@gmail.com>
2018-03-14 12:16:13 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,14 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.status(200).send({ todos });
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
