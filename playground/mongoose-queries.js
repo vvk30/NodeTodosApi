6a403c54ca60aa339db202989adc276aa0a8aa64
Vishal Kulkarni <vishal.kulkarni303@gmail.com>
2018-03-15 14:32:32 +0530

    Add DELETE /todos/:id

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -11,18 +11,21 @@ if (!ObjectID.isValid(id)) {
     return console.log('Id not valid');
 }
 
+//Find todos
 Todo.find({
     _id: id
 }).then((todos) => {
     console.log('Todos', todos);
 }).catch((e) => console.log(e));
 
+//Find one 
 Todo.findOne({
     _id: id
 }).then((todo) => {
     console.log('Todo', todo);
 }).catch((e) => console.log(e));
 
+//Find by Id
 Todo.findById(id).then((todo) => {
     if (!todo) {
         return console.log('ID not found');
@@ -30,9 +33,26 @@ Todo.findById(id).then((todo) => {
     console.log('TodoById:' + todo);
 }).catch((e) => console.log(e));
 
+//Find user by Id
 User.findById(uid).then((user) => {
     if (!user) {
         return console.log('No user found');
     }
     console.log('User' + user);
 }).catch((e) => console.log(e));
+
+//Remove all Todos
+Todo.remove({}).then((res) => {
+    console.log(res);
+});
+
+//Find one and remove 
+Todo.findOneAndRemove({ _id: '5aa8ff0f3cd9ff24a4eeba39' }).then((res) => {
+    console.log(res);
+})
+
+//Find by id and remove
+
+Todo.findByIdAndRemove('5aa8ff0f3cd9ff24a4eeba39').then((res) => {
+    console.log(res);
+});
--- server/server.js
+++ server/server.js
@@ -46,6 +46,20 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+app.delete('/todos/:id', (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) {
+        res.status(400).send();
+    }
+    Todo.findByIdAndRemove(req.params.id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send(todo);
+    }).catch((e) => {
+        res.status(404).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
